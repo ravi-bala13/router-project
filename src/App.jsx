4bb35680db075fe1abfee144b0e426d6c0c521d0
Bala Murugan R <bals161616@gmail.com>
2022-03-23 20:23:44 +0530

    change in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<Home />} /> */}
+      </Routes>
     </div>
   );
 }
